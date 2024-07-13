c04815c9a8720c44111e1395d02b11ca6773785e
Mohamed Ghazal <medghazalcs@gmail.com>
2024-07-13 14:40:10 +0200

    Refractor code und remove duplication/Add .sh scripts for ease of use

---
+++ runExtractor.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+echo 'TEST'
+cd ~/Developer/readExportElixo/
+pwd
+source venv/bin/activate
+python3 script.py
--- script.py
+++ script.py
@@ -4,8 +4,68 @@ from bs4 import BeautifulSoup
 from csv import DictWriter
 
 
+FIELDNAMES = [
+    '#',
+    'ID loueur',
+    'Compte',
+    'Nom du loueur',
+    'Nombre de dossiers',
+    'Solde CRM (A)',
+    'Solde compta (B)',	
+    'Facture achat / avoir (C)',
+    'Différence (A-B-C)',
+    'Statut compte',
+    'Commentaires',
+    'Référence',
+    'Client',
+    'Date de réservation',
+    'Date de départ',
+    'Tags',
+    'Reste à payer',
+    'Statut dossier',
+]
+
+
 def get_text(element):
     return element.p.span.get_text()
+
+
+def dectify_data(fieldnames, tds):
+    return {
+        key: get_text(td)
+        for key, td
+        in zip(
+            fieldnames[0:11],
+            [td for td in tds],
+        )
+    }
+
+
+def processHTMLFile(html_document, target_file):
+    soup = BeautifulSoup(html_document, 'html.parser')
+    table = soup.table.tbody
+
+    with open(target_file, 'w') as csvFile:
+        writer = DictWriter(csvFile, fieldnames=FIELDNAMES)
+        writer.writeheader()
+        lines = table.find_all('tr')
+        for i in range(len(lines)):
+            row_dict = {}
+            tds = lines[i].find_all('td')
+            if get_text(tds[0]) == '#':
+                tds = lines[i + 1].find_all('td')
+                lessor_data = dectify_data(FIELDNAMES[0:11], tds)
+                k = 3
+                while i + k < len(lines):
+                    tds = lines[i + k].find_all('td')
+                    if get_text(tds[0]) == '#':
+                        k += 1
+                        break
+                    k += 1
+                    row_dict = dectify_data(FIELDNAMES[11:], tds[1:])
+                    row_dict.update(lessor_data)
+                    writer.writerow(row_dict)
+                i += k
 
 
 if __name__ == '__main__':
@@ -17,62 +77,9 @@ if __name__ == '__main__':
 
         if file.split('.')[1] == 'webarchive':
             run(['textutil', '-convert', 'html', file])
+            run(['rm', file])
 
             with open(f'{file.split(".")[0]}.html', 'r') as htmlFile:
-                html_doc = htmlFile.read()
-                soup = BeautifulSoup(html_doc, 'html.parser')
-                table = soup.table.tbody
-                with open('output.csv', 'w') as csvFile:
-                    fieldnames = [
-                        '#',
-                        'ID loueur',
-                        'Compte',
-                        'Nom du loueur',
-                        'Nombre de dossiers',
-                        'Solde CRM (A)',
-                        'Solde compta (B)',	
-                        'Facture achat / avoir (C)',
-                        'Différence (A-B-C)',
-                        'Statut compte',
-                        'Commentaires',
-                        'Référence',
-                        'Client',
-                        'Date de réservation',
-                        'Date de départ',
-                        'Tags',
-                        'Reste à payer',
-                        'Statut dossier',
-                    ]
-                    writer = DictWriter(csvFile, fieldnames=fieldnames)
-                    writer.writeheader()
-                    lines = table.find_all('tr')
-                    for i in range(len(lines)):
-                        rowDict = {}
-                        tds = lines[i].find_all('td')
-                        if get_text(tds[0]) == '#':
-                            tds = lines[i + 1].find_all('td')
-                            lessorData = {
-                                key: value
-                                for key, value
-                                in zip(
-                                    fieldnames[0:11],
-                                    [get_text(td) for td in tds],
-                                )
-                            }
-                            k = 3
-                            while True:
-                                if i + k >= len(lines):
-                                    break
-                                tds = lines[i + k].find_all('td')
-                                if get_text(tds[0]) == '#':
-                                    k += 1
-                                    break
-                                k += 1
-                                rowDict = {
-                                    key: get_text(value)
-                                    for key, value
-                                    in zip(fieldnames[11:], tds[1:])
-                                }
-                                rowDict.update(lessorData)
-                                writer.writerow(rowDict)
-                            i += k
+                processHTMLFile(htmlFile.read(), f'{file.split(".")[0]}.csv')
+
+            run(['rm', f'{file.split(".")[0]}.html'])
